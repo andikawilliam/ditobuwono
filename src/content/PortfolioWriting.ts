980d54a1d67f52ac283553bd2f2ef436b5f61fa3
hanindito <74034463+hanindito@users.noreply.github.com>
2021-04-14 22:27:31 +0700

    Update PortfolioWriting.ts

--- src/content/PortfolioWriting.ts
+++ src/content/PortfolioWriting.ts
@@ -39,7 +39,6 @@ export default Vue.extend({
           the feelings that he has felt during his life until now.
           `,
           accolades: [
-            "Indie Book Corner Best Sellers List",
             "Editors' Choice at bukuindie.com",
             "Book Talks at “Bedah Buku Madania“"
           ],
